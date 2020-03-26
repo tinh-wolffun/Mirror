5c22fb4deffe657b33c572d18cf3c9e6d956d0ce
mteagle <kai260698@gmail.com>
2020-03-26 11:13:15 +0700

    first Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] private Vector3 movement = new Vector3();
+
+    [Client]
+    void Update()
+    {
+        if (!hasAuthority) { return; }
+
+        if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+
+        //transform.Translate(movement);
+        CmdMove();
+    }
+    [Command]
+    private void CmdMove()
+    {
+        RpcMove();
+    }
+
+    [ClientRpc]
+    private void RpcMove() => transform.Translate(movement);
+}
---
+++ Assets/Scripts/RandomizePosition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizePosition : MonoBehaviour
+{
+    public Transform childS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(
+            Random.Range(-4f, 4f), Random.Range(-4f, 4f), 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.up*3;
+            childS.position = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += Vector3.down*3;
+            childS.position = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left*3;
+            childS.position = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += Vector3.right*3;
+            childS.position = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0);
+        }
+
+    }
+}
